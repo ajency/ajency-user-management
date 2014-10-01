77c8dd7736c3ca14ac3f88734e2f11e9b59f4898
Suraj Air <suraj@ajency.in>
2014-10-01 15:59:08 +0530

    plugin skeleton added

---
+++ ajency-user-management.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Ajency User Management
+ *
+ * Everything from registration, forgot password, user activation, role and capabilities, user level backups, restores etc...
+ *
+ * @package   ajency-user-management
+ * @author    Team Ajency <team@ajency.in>
+ * @license   GPL-2.0+
+ * @link      http://ajency.in
+ * @copyright 10-1-2014 Ajency.in
+ *
+ * @wordpress-plugin
+ * Plugin Name: Ajency User Management
+ * Plugin URI:  http://ajency.in
+ * Description: Everything from registration, forgot password, user activation, role and capabilities, user level backups, restores etc...
+ * Version:     0.1.0
+ * Author:      Team Ajency
+ * Author URI:  http://ajency.in
+ * Text Domain: ajency-user-management-locale
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: /lang
+ */
+
+// If this file is called directly, abort.
+if (!defined("WPINC")) {
+	die;
+}
+
+require_once(plugin_dir_path(__FILE__) . "AjencyUserManagement.php");
+
+// Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
+register_activation_hook(__FILE__, array("AjencyUserManagement", "activate"));
+register_deactivation_hook(__FILE__, array("AjencyUserManagement", "deactivate"));
+
+AjencyUserManagement::get_instance();
---
+++ js/ajency-user-management-admin.js
@@ -0,0 +1,19 @@
+/**
+ * This is the main javascript file for the Ajency User Management plugin's main administration view.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end administrator.
+ *
+ * @package   ajency-user-management
+ * @author    Team Ajency <team@ajency.in>
+ * @license   GPL-2.0+
+ * @link      http://ajency.in
+ * @copyright 10-1-2014 Ajency.in
+ */
+
+(function ($) {
+	"use strict";
+	$(function () {
+		// Place your administration-specific JavaScript here
+	});
+}(jQuery));
